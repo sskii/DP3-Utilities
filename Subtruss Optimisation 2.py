44337ef70007652086b103e3281d54627c245be8
Sam <30613658+sskii@users.noreply.github.com>
2021-05-19 17:56:27 +1200

    Changed execution parameters

--- Subtruss Optimisation 2.py
+++ Subtruss Optimisation 2.py
@@ -64,7 +64,7 @@ def evaluateSubtruss(length, triangles, precision):
 # test code #
 triangles = [2, 4, 6, 8]	# possible numbers of triangles
 							# trusses to test
-subtrusses = [191.169, 261.600, 111.111]
+subtrusses = [220, 267]
 
 for i in subtrusses:
 	print("\nSolutions for", i, "mm subtruss:\nTriangles:  Half of enclosed angle:  Capacity (diags, verts)^  Length (diags, verts)")
